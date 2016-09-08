799d70405e76e5ed4f9bbd6fd1d4f47ff5929c8a
Alexey Demedetskiy <alexey.demedetskiy@sigma.software>
2016-09-08 14:39:10 +0100

    Initial

---
+++ Hack/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Hack
+//
+//  Created by Alexey Demedetskii on 9/8/16.
+//  Copyright © 2016 thebestteamevermade. All rights reserved.
+//
+
+import UIKit
+import Moscapsule
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func dance() {
+        let mqttConfig = MQTTConfig(clientId: "test", host: "192.168.1.107", port: 1883, keepAlive: 60)
+        mqttConfig.onPublishCallback = { messageId in
+            NSLog("published (mid=\(messageId))")
+        }
+        mqttConfig.onMessageCallback = { mqttMessage in
+            NSLog("MQTT Message received: payload=\(mqttMessage.payloadString)")
+        }
+        mqttConfig.onConnectCallback = { x in
+            print("Connected!!! \(x)")
+        }
+        
+        // create new MQTT Connection
+        let mqttClient = MQTT.newConnection(mqttConfig)
+        
+        // publish and subscribe
+        mqttClient.publishString("{\"Left\": -4000,\"Right\": 4000}", topic: "command/wheel_speed", qos: 2, retain: true)
+        
+        
+    }
+}
+
---
+++ Pods/Target Support Files/Pods-Hack/Pods-Hack-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_HackVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_HackVersionString[];
+
